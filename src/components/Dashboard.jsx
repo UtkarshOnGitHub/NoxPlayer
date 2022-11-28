2948e257d79eded28acb7680b2f36fb7188c39cf
Utkarsh <famedustevershine@gmail.com>
2022-11-28 21:17:59 +0530

    Done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,19 +7,39 @@ import song3 from "../Assets/Song1.wav";
 let newAudio = new Audio(song1)
 
 let arr = [
-    {songTitle:"Anamoly", audioSrc:song1 ,imgSrc:"",artist:"Noxious"},
-    {songTitle:"Anamoly", audioSrc:song2 ,imgSrc:"",artist:"Noxious"},
-    {songTitle:"Anamoly", audioSrc:song3 ,imgSrc:"",artist:"Noxious"},
+    {songTitle:"Anamoly", audioSrc:song1 ,imgSrc:"https://www.whoa.in/download/cool-trending-mobile-wallpapers-hd-wallpapers-images",artist:"Noxious"},
+    {songTitle:"Gunna Type Beat", audioSrc:song2 ,imgSrc:"https://w0.peakpx.com/wallpaper/322/439/HD-wallpaper-x-xten-blue-cool-design-rap-rapper-red.jpg",artist:"Jay Cactus"},
+    {songTitle:"Reason To Kill You", audioSrc:song3 ,imgSrc:"https://play-lh.googleusercontent.com/ISSpyeMde6MqPGYcrFN5tQyRUtPX903MDuwiQuLqXGOVNDX9Bt0a34iwnkK16b2lFxU",artist:"Ocean"},
 ]
-
+let i =0 
+const fixTime = (time) => {
+    if(!time){
+        return `00`
+    }
+    return time > 10 ? time : "0" + time;
+  };
+  
+  const formatTime = (time) => {
+    const seconds = time % 60;
+    const min = Math.floor(time / 60) % 60;
+    const hours = Math.floor(time / (60 * 60));
+    return `${fixTime(min)}:${fixTime(seconds)}`;
+  };
 const Dashboard = () => {
-    
+
+    let time = formatTime(Math.floor(newAudio.duration)) || `0:00`
+    const [Currtime , setCurrTime] = useState(time)
     const [playState , setPlayState] = useState(false)
     const [progressValue , setProgressValue] = useState(0/100)
+    const [title,setTitle] = useState(arr[0].songTitle)
+    const [artist,setArtist] = useState(arr[0].artist)
+    const [image ,setImage] = useState(arr[0].audioSrc)
 
     newAudio.addEventListener("timeupdate",()=>{
         let progress = Math.floor((newAudio.currentTime/newAudio.duration)*100)
         setProgressValue(progress)
+        let timeZone = formatTime(Math.floor(newAudio.currentTime)) || `0:00`
+        setCurrTime(timeZone)
         if(newAudio.currentTime===newAudio.duration){
             setPlayState(!playState)
         }
@@ -39,17 +59,56 @@ const Dashboard = () => {
 
     }
 
+    const handleForward =()=>{
+        if(i==arr.length-1){
+            i=0
+        }else{
+            i++
+        }
+        newAudio.pause();
+        setPlayState(false)
+        newAudio = new Audio(arr[i].audioSrc);
+        setTimeout(()=>{
+            setTitle(arr[i].songTitle)
+            setArtist(arr[i].artist)
+            setImage(arr[i].imgSrc)
+            newAudio.play()
+            setPlayState(true)
+        },500)
+
+    }
+
+        const handleBackward =()=>{
+            if(i==0){
+                i = arr.length
+            }
+            i--
+            newAudio.pause()
+            setPlayState(false)
+            newAudio = new Audio(arr[i].audioSrc);
+            setTimeout(()=>{
+                setArtist(arr[i].artist)
+                setTitle(arr[i].songTitle)
+                setImage(arr[i].imgSrc)
+                newAudio.play()
+                setPlayState(true)
+            },500)
+
+        }
+
   return (
     <div className="player-container">
         <div className="img-container">
-            <img src="https://www.whoa.in/download/cool-trending-mobile-wallpapers-hd-wallpapers-images" alt="Album Art"/>
+            <img src={image} alt="Album Art"/>
         </div>
         <div className="data">
-            <Text className="title">White Linens</Text>
-            <Text className="artist">Vangard</Text>
+            <Text className="title">{title}</Text>
+            <Text className="artist">{artist}</Text>
         </div>
         <div className="progress-container" id="progress-container">
             <Slider aria-label='slider-ex-4' defaultValue={0} value={progressValue} onChange={(val) => {handleProgressChange(val)}}>
+                <SliderMark value={0} mt='1' ml='-2.5' fontSize='sm'>{Currtime}</SliderMark>
+                <SliderMark value={95} mt='1' ml='-2.5' fontSize='sm'>{time}</SliderMark>
                 <SliderTrack bg='red.100'>
                     <SliderFilledTrack bg='tomato' />
                 </SliderTrack>
@@ -60,7 +119,7 @@ const Dashboard = () => {
         </div>
         
         <div className="player-controls">
-            <BsFillSkipBackwardFill className='fas' />
+            <BsFillSkipBackwardFill className='fas' onClick={handleBackward} />
             {!playState ? <BsPlayFill className='fas' onClick={handlePlaySong}/>:<BsPauseFill className='fas' onClick={handlePauseSong}/>}
             <BsFillSkipForwardFill className='fas' onClick={handleForward}/>
         </div>
